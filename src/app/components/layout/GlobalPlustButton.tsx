1b3bb5feab758b46624088974ae2e7f45a64f293
nakyeonko3 <badaclock@gmail.com>
2025-05-22 11:15:01 +0900

    design: MainLayout에 플러스 버튼 추가

---
+++ src/app/components/layout/GlobalPlustButton.tsx
@@ -0,0 +1,14 @@
+'use client';
+import PlusButton from '../common/plusButton';
+
+export default function GlobalPlusButton() {
+  const handleClick = () => {
+    console.log('handleClick');
+  };
+
+  return (
+    <div className="fixed bottom-4 right-4">
+      <PlusButton onClick={handleClick} />
+    </div>
+  );
+}
