448db69e6d379af0c17642de061b9fe406ac17c6
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-05-23 15:10:25 +0900

    fix: add search icon to search bar

--- src/app/components/common/SearchBar.tsx
+++ src/app/components/common/SearchBar.tsx
@@ -1,10 +1,14 @@
+import { MdSearch } from 'react-icons/md';
+
 export default function SearchBar({}) {
   return (
-    <div className="flex items-center w-full">
+    <div className="flex items-center w-full border-1 border-gray-300 px-2 py-2 rounded-full">
+      <MdSearch size={24} className="relative left-1" />
       <input
         type="text"
+        role="search"
         placeholder="Search..."
-        className="border border-gray-300 rounded-full px-4 py-2 w-full"
+        className="border-none outline-none relative left-3 w-full"
         aria-label="Search"
       />
     </div>
