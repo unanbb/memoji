5f986efde791b89b2fd192702e12964032f56e3c
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-05-25 17:29:15 +0900

    design: ToggleButton 색상을 및 props 수정
    - gray-700으로 변경
    - className prop 추가

--- src/components/common/ToggleButton.tsx
+++ src/components/common/ToggleButton.tsx
@@ -6,15 +6,15 @@ interface ToggleButtonProps {
   isOpen?: boolean;
 }
 
-export default function ToggleButton({ onClick, isOpen }: ToggleButtonProps) {
+export default function ToggleButton({ onClick, isOpen, className }: ToggleButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="bg-none border-none text-2xl transition-transform duration-300"
+      className={`bg-none border-none text-2xl transition-transform duration-300 ${className}`}
       type="button"
     >
       <FaChevronDown
-        className={`transition-transform duration-300 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
+        className={`transition-transform bg-gray-700 duration-300 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
       />
     </button>
   );
