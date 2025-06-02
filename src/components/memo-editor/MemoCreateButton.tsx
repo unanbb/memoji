5e8cffae475647e0e8ac61b2a9ef0b0dd4472c98
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-06-02 11:54:23 +0900

    fix: MemoCreateModal 컴포넌트 상태 관리 개선
    - MemoCreateModal 컴포넌트에서 메모 생성 로직을 action.ts로 분리하여 서버 액션으로 처리
    - PlusButton 컴포넌트에 aria-label 추가
    - MemoCreateButton 컴포넌트에서 모달 닫기/모달 열기 로직 상태만 관리 하도록 설정

--- src/components/memo-editor/MemoCreateButton.tsx
+++ src/components/memo-editor/MemoCreateButton.tsx
@@ -1,66 +1,21 @@
 'use client';
 
-import { fetchCreateMemo } from '@/action';
 import PlusButton from '@/components/common/PlusButton';
 import MemoCreateModal from '@/components/memo-editor/MemoCreateModal';
-import type { MemoProps } from '@/types/memo';
 import { useCallback, useState } from 'react';
 
 export default function MemoCreateButton() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [memoData, setMemoData] = useState<Omit<MemoProps, 'id' | 'createdAt'>>({
-    title: '',
-    content: '',
-    category: '',
-  });
-
-  const saveMemo = useCallback(async () => {
-    if (!memoData.title || !memoData.content || !memoData.category) {
-      console.error('메모 제목, 내용, 카테고리는 필수입니다.');
-      return;
-    }
-    try {
-      const response = await fetchCreateMemo(memoData);
-      console.log('메모가 성공적으로 생성되었습니다:', response);
-    } catch (error) {
-      console.error('메모 생성 중 오류 발생:', error);
-      //TODO: 사용자에게 오류 메시지를 표시하는 로직 추가 필요
-    }
-    setMemoData({
-      title: '',
-      content: '',
-      category: '',
-    });
-  }, [memoData]);
 
   const openModal = useCallback(() => {
     setIsModalOpen(true);
-    setMemoData({
-      title: '',
-      content: '',
-      category: '',
-    });
   }, []);
-
-  const hideModal = useCallback(() => {
-    setIsModalOpen(false);
-    saveMemo();
-  }, [saveMemo]);
-
-  const handlePlusButtonClick = () => {
-    openModal();
-  };
 
   return (
     <div className="fixed bottom-4 right-4 z-50">
-      <MemoCreateModal
-        isOpen={isModalOpen}
-        onClose={hideModal}
-        memoData={memoData}
-        setMemoData={setMemoData}
-      />
+      {isModalOpen && <MemoCreateModal />}
       <div className="absolute top-2 right-2"></div>
-      <PlusButton onClick={handlePlusButtonClick} />
+      <PlusButton onClick={openModal} label="메모 추가" />
     </div>
   );
 }
--- src/components/memo-editor/MemoCreateModal.tsx
+++ src/components/memo-editor/MemoCreateModal.tsx
@@ -1,29 +1,48 @@
+import { fetchCreateMemo } from '@/action';
 import CrossButton from '@/components/common/CrossButton';
 import InputField from '@/components/common/InputField';
 import MarkDownEditor from '@/components/MarkdownEditor';
 import { Modal } from '@/components/Modal';
 import type { MemoProps } from '@/types/memo';
+import { useCallback, useState } from 'react';
 
-export default function MemoCreateModal({
-  isOpen,
-  onClose,
-  memoData,
-  setMemoData,
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-  memoData: Omit<MemoProps, 'id' | 'createdAt'>;
-  setMemoData: React.Dispatch<React.SetStateAction<Omit<MemoProps, 'id' | 'createdAt'>>>;
-}) {
+export default function MemoCreateModal() {
+  const [memoData, setMemoData] = useState<Omit<MemoProps, 'id' | 'createdAt'>>({
+    title: '',
+    content: '',
+    category: '',
+  });
+
+  const submitMemo = useCallback(async () => {
+    if (!memoData.title || !memoData.content || !memoData.category) {
+      console.error('메모 제목, 내용, 카테고리는 필수입니다.');
+      return;
+    }
+    try {
+      const response = await fetchCreateMemo(memoData);
+      console.log('메모가 성공적으로 생성되었습니다:', response);
+    } catch (error) {
+      console.error('메모 생성 중 오류 발생:', error);
+      //TODO: 사용자에게 오류 메시지를 표시하는 로직 추가 필요
+    }
+    setMemoData({
+      title: '',
+      content: '',
+      category: '',
+    });
+  }, [memoData]);
+
   return (
     <Modal
-      isOpen={isOpen}
-      onClose={onClose}
+      onClose={submitMemo}
+      isOpen={true}
+      aria-label="메모 생성"
+      aria-labelledby="memo-create-modal"
       size="large"
       className="max-w-2xl relative sm:h-[70vh] h-[100vh]"
     >
       <div className="absolute top-1 right-1">
-        <CrossButton onClick={onClose} label="Close editor" />
+        <CrossButton onClick={submitMemo} label="Close editor" />
       </div>
       <div className="mb-2 mt-2">
         <InputField
