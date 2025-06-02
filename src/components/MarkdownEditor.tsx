94e01d327efc7debeb7cea24f8493ac8d13b696c
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-06-02 12:14:44 +0900

    fix: MarkdownEditor 스타일과 모바일 반응형 개선
    - 화면 리사이징 이벤트를 제거하고, 모바일 크기 체크를 간소화

--- src/components/MarkdownEditor.tsx
+++ src/components/MarkdownEditor.tsx
@@ -2,7 +2,6 @@
 import '@uiw/react-markdown-preview/markdown.css';
 import '@uiw/react-md-editor/markdown-editor.css';
 import dynamic from 'next/dynamic';
-import { Suspense, useCallback, useEffect, useState } from 'react';
 
 const MDEditor = dynamic(() => import('@uiw/react-md-editor'), { ssr: false });
 
@@ -13,47 +12,28 @@ interface MarkDownEditorProps {
 }
 
 export default function MarkDownEditor({ value, onChange, markDownProps }: MarkDownEditorProps) {
-  const mobileSize = 768;
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= mobileSize);
-
-  const checkIsMobile = useCallback(() => {
-    setIsMobile(window.innerWidth <= mobileSize);
-  }, [mobileSize]);
-
-  useEffect(() => {
-    checkIsMobile();
-    window.addEventListener('resize', checkIsMobile);
-    return () => {
-      window.removeEventListener('resize', checkIsMobile);
-    };
-  }, [checkIsMobile]);
-
+  const isMobile = window.innerWidth <= 768;
   const editorStyle = isMobile
     ? {
         height: '100%',
-        containerHeight: '78vh',
         preview: 'edit',
       }
     : {
         height: '50vh',
-        containerHeight: '50vh',
         preview: 'live',
       };
 
   return (
-    <div data-color-mode="light" style={{ height: editorStyle.containerHeight }}>
-      <Suspense fallback={<div>Loading editor...</div>}>
-        <MDEditor
-          value={value}
-          onChange={onChange}
-          height={editorStyle.height}
-          style={{ height: editorStyle.height }}
-          preview={editorStyle.preview === 'live' ? 'live' : 'edit'}
-          autoFocus
-          autoFocusEnd
-          {...markDownProps}
-        />
-      </Suspense>
-    </div>
+    <MDEditor
+      data-color-mode="light"
+      value={value}
+      onChange={onChange}
+      height={editorStyle.height}
+      style={{ height: editorStyle.height }}
+      preview={editorStyle.preview === 'live' ? 'live' : 'edit'}
+      autoFocus
+      autoFocusEnd
+      {...markDownProps}
+    />
   );
 }
