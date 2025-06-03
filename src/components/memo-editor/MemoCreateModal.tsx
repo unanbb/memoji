dee9b8987e1ac9ae2b5dfba4736c162e1c62b15f
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-06-03 10:31:01 +0900

    feat: MemoCreateModal - 카테고리 추가 기능 구현중
    - 이 커밋은 MemoCreateModal 컴포넌트에 카테고리 추가 기능을 구현중
    - + 버튼 클릭 시 카테고리 모달이 열리고, 카테고리를 추가할 수 있는 기능을 추가할 예정

--- src/components/memo-editor/MemoCreateModal.tsx
+++ src/components/memo-editor/MemoCreateModal.tsx
@@ -1,5 +1,7 @@
 'use client';
 import { fetchCreateMemo } from '@/action';
+import CategoryModal from '@/components/category/CategoryModal';
+import AddButton from '@/components/common/AddButton';
 import CrossButton from '@/components/common/CrossButton';
 import InputField from '@/components/common/InputField';
 import MarkDownEditor from '@/components/MarkdownEditor';
@@ -22,7 +24,7 @@ export default function MemoCreateModal({ onClose }: MemoCreateModalProps) {
   const router = useRouter();
   const submitMemo = useCallback(async () => {
     onClose();
-    if (!memoData.title || !memoData.content || !memoData.category) {
+    if (!memoData.title || !memoData.content) {
       console.error('메모 제목, 내용, 카테고리는 필수입니다.');
       return;
     }
@@ -40,6 +42,12 @@ export default function MemoCreateModal({ onClose }: MemoCreateModalProps) {
     });
     router.push('/');
   }, [memoData, onClose, router]);
+
+  const [isOpenCategoryModal, setIsOpenCategoryModal] = useState(false);
+  // 카테고리 모달 열기
+  const openCategoryModal = useCallback(() => {
+    setIsOpenCategoryModal(true);
+  }, []);
 
   return (
     <Modal
@@ -63,13 +71,26 @@ export default function MemoCreateModal({ onClose }: MemoCreateModalProps) {
         />
       </div>
 
-      <div className="mb-4">
+      <div className="mb-4 relative">
         <InputField
           placeholder="카테고리"
           label="메모 카테고리"
           value={memoData.category}
           onChange={e => setMemoData(prev => ({ ...prev, category: e.target.value }))}
         />
+        <AddButton
+          onClick={openCategoryModal}
+          className="absolute -top-1 right-0"
+          label="카테고리 추가"
+        />
+        {isOpenCategoryModal && (
+          <div className="absolute top-4 right-1 z-50">
+            <CategoryModal
+              categories={['기타', '개발', '일상', '공부']}
+              onClose={() => setIsOpenCategoryModal(false)}
+            />
+          </div>
+        )}
       </div>
       <MarkDownEditor
         value={memoData.content}
