d8961c5609c79d013e7bfeb597cbec7f274f7ab4
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-06-05 11:44:43 +0900

    feat: react-toastify 의존성 설치 및 MainLayout에 추가

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/layout/Header';
 import Sidebar from '@/components/layout/sidebar/Sidebar';
 import MemoCreateButton from '@/components/memo-editor/MemoCreateButton';
 import { useState } from 'react';
+import { ToastContainer } from 'react-toastify';
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -20,7 +21,10 @@ export default function MainLayout({ children }: MainLayoutProps) {
     <div className="flex flex-row min-h-screen relative">
       <div className="flex-1 flex flex-col min-h-screen w-full md:w-auto">
         <Header onToggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
-        <main className="flex-1 container mx-auto px-4 py-3 mt-16">{children}</main>
+        <main className="flex-1 container mx-auto px-4 py-3 mt-16">
+          {children}
+          <ToastContainer />
+        </main>
       </div>
       <Sidebar isOpen={isSidebarOpen} onToggle={toggleSidebar} className="md:static absolute" />
       <MemoCreateButton />
