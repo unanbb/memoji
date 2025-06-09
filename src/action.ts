0f98b4073d34390d2b26f4834d7de6a4b5f28e21
LeeYunHwan <cyzhqly@gmail.com>
2025-06-09 20:39:46 +0900

    feat: 카테고리 수정 api 연동 & toast 메시지 연동 #50

--- src/action.ts
+++ src/action.ts
@@ -60,3 +60,27 @@ export const fetchDeleteCategory = async (categoryName: string) => {
     throw new Error('카테고리 삭제 중 오류가 발생했습니다.');
   }
 };
+
+export const fetchModifyCategory = async (newCategoryName: string) => {
+  try {
+    const original = newCategoryName;
+    const response = await fetch('http://localhost:3000/api/categories/', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        newName: {newCategoryName},
+      }),
+    });
+
+    if(!response.ok){
+      throw new Error('카테고리 수정에 실패했습니다.');
+    }
+
+    console.log(`카테고리명 수정 성공! ${original} -> ${newCategoryName}`)
+  } catch (error) {
+    console.error('카테고리 수정 중 오류 발생: ', error);
+    throw new Error('카테고리 수정 중 오류가 발생했습니다.');
+  }
+};
