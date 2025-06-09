2e9ab0c0c6ee09c64c3ed5d8af9a0ffd5a2cabc8
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-06-09 16:38:25 +0900

    fix: 공통된 getQueryClient 로직을 사용하여 쿼리 클라이언트 생성
    - 서버와 브라우저 환경에서 쿼리 클라이언트를 통합된 로직으로 생성하도록 변경

--- src/app/getQueryClient.tsx
+++ src/app/getQueryClient.tsx
@@ -1,6 +1,34 @@
-import { QueryClient } from '@tanstack/react-query';
+import { QueryClient, isServer } from '@tanstack/react-query';
 import { cache } from 'react';
 
-// cache() is scoped per request, so we don't leak data between requests
-const getQueryClient = cache(() => new QueryClient());
-export default getQueryClient;
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        // With SSR, we usually want to set some default staleTime
+        // above 0 to avoid refetching immediately on the client
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    // Server: always make a new query client
+    // cache() is scoped per request, so we don't leak data between requests
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    // This is very important, so we don't re-make a new client if React
+    // suspends during the initial render. This may not be needed if we
+    // have a suspense boundary BELOW the creation of the query client
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+// cache() is scoped per request for server-side rendering
+export default cache(getQueryClient);
