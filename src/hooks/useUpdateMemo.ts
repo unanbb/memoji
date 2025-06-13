534f8408584327ca64a17c77c3e2f43d44662c18
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-06-13 20:12:08 +0900

    fix: useUpdateMemo에 optimistic update 기능 구현
    - useUpdateMemo 훅에 onMutate를 통해 optimistic update 기능을 추가
    - 이전 메모와 메모 리스트를 저장하고, 업데이트 후에 해당 데이터를 업데이트하도록 수정

--- src/hooks/useUpdateMemo.ts
+++ src/hooks/useUpdateMemo.ts
@@ -35,9 +35,40 @@ export default function useUpdateMemo() {
   const mutation = useMutation({
     mutationFn: (params: { id: string; memo: Omit<MemoProps, 'createdAt'> }) =>
       fetchUpdateMemo(params.id, params.memo),
-    onSuccess: (_data, variables) => {
-      queryClient.invalidateQueries({ queryKey: queryKeys.memo.detail(variables.id) });
-      queryClient.invalidateQueries({ queryKey: queryKeys.memo.lists() });
+    onMutate: async variables => {
+      await queryClient.cancelQueries({ queryKey: queryKeys.memo.detail(variables.id) });
+      await queryClient.cancelQueries({ queryKey: queryKeys.memo.lists() });
+      const previousMemo = queryClient.getQueryData<MemoProps>(queryKeys.memo.detail(variables.id));
+      const previousMemos = queryClient.getQueryData<MemoProps[]>(queryKeys.memo.lists());
+
+      if (!previousMemo || !previousMemos) return { previousMemo, previousMemos };
+
+      queryClient.setQueryData<MemoProps[]>(queryKeys.memo.lists(), old => {
+        if (!old) return old;
+        return old.map(memo => (memo.id === variables.id ? { ...memo, ...variables.memo } : memo));
+      });
+
+      queryClient.setQueryData<MemoProps>(queryKeys.memo.detail(variables.id), old => {
+        if (!old) return old;
+        return {
+          ...old,
+          ...variables.memo,
+        };
+      });
+      return { previousMemo, previousMemos };
+    },
+    onError: (error, variables, context) => {
+      if (context?.previousMemo) {
+        queryClient.setQueryData<MemoProps>(
+          queryKeys.memo.detail(variables.id),
+          context.previousMemo,
+        );
+      }
+      if (context?.previousMemos) {
+        queryClient.setQueryData<MemoProps[]>(queryKeys.memo.lists(), context.previousMemos);
+      }
+      console.error('Error updating memo:', error);
+      throw error;
     },
   });
 
