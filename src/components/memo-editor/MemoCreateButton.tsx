dc911a1b4bf61e1d1b41c010ecf88f3a72046ce9
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-06-14 21:36:26 +0900

    fix: 로그인 상태에서만 메모 추가 버튼 표시

--- src/components/memo-editor/MemoCreateButton.tsx
+++ src/components/memo-editor/MemoCreateButton.tsx
@@ -2,10 +2,12 @@
 
 import PlusButton from '@/components/common/PlusButton';
 import MemoCreateModal from '@/components/memo-editor/MemoCreateModal';
+import { useAuth } from '@/hooks/useAuth';
 import { useCallback, useState } from 'react';
 
 export default function MemoCreateButton() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { isAuthenticated } = useAuth();
 
   const onClose = useCallback(() => {
     setIsModalOpen(false);
@@ -16,10 +18,14 @@ export default function MemoCreateButton() {
   }, []);
 
   return (
-    <div className="fixed bottom-4 right-4 z-49">
-      {isModalOpen && <MemoCreateModal onClose={onClose} />}
-      <div className="absolute top-2 right-2"></div>
-      <PlusButton onClick={openModal} label="메모 추가" />
-    </div>
+    <>
+      {isAuthenticated && (
+        <div className="fixed bottom-4 right-4 z-49">
+          {isModalOpen && <MemoCreateModal onClose={onClose} />}
+          <div className="absolute top-2 right-2"></div>
+          <PlusButton onClick={openModal} label="메모 추가" />
+        </div>
+      )}
+    </>
   );
 }
