2cd06d498dd2ac629dda2681dbc6c89a7fc0a1c4
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-06-15 17:20:46 +0900

    fix: usePostMemo hook 에 optimistic UI 적용
    - usePostMemo 훅에 optimistic UI를 적용하여 메모 생성 시 사용자 경험을 개선
    - 메모 생성 시 임시 ID와 현재 시간을 사용하여 즉시 UI에 반영
    - 이후 실제 데이터베이스에 저장된 메모와 동기화

--- src/hooks/usePostMemo.ts
+++ src/hooks/usePostMemo.ts
@@ -1,6 +1,7 @@
 import { queryKeys } from '@/lib/queryKeys';
 import type { MemoProps } from '@/types/memo';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { Timestamp } from 'firebase/firestore';
 
 export const fetchCreateMemo = async (memoData: Omit<MemoProps, 'id' | 'createdAt'>) => {
   try {
@@ -30,10 +31,33 @@ export default function usePostMemo() {
   const queryClient = useQueryClient();
   const { mutate, isPending } = useMutation({
     mutationFn: fetchCreateMemo,
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: queryKeys.memo.lists(),
-      });
+    onMutate: async newMemo => {
+      await queryClient.cancelQueries({ queryKey: queryKeys.memo.lists() });
+      const previousMemos = queryClient.getQueryData<MemoProps[]>(queryKeys.memo.lists());
+
+      const newOptimisticMemo: MemoProps = {
+        ...newMemo,
+        id: `Temp-${crypto.randomUUID()}`,
+        category: newMemo?.category || 'others',
+        createdAt: Timestamp.now(),
+      };
+
+      if (previousMemos) {
+        queryClient.setQueryData<MemoProps[]>(queryKeys.memo.lists(), old => {
+          if (!old) return [newOptimisticMemo];
+          return [newOptimisticMemo, ...old];
+        });
+      }
+      return { previousMemos };
+    },
+    onError: (error, newMemo, context) => {
+      queryClient.setQueryData<MemoProps[]>(queryKeys.memo.lists(), context?.previousMemos);
+      console.error('메모 생성 중 오류 발생:', error);
+    },
+    onSettled: () => {
+      // TODO: 전체 메모 목록을 리패치하는 대신, 새로 생성된 메모만 추가하는 방법을 고려하기
+      queryClient.refetchQueries({ queryKey: queryKeys.memo.lists() });
+      queryClient.refetchQueries({ queryKey: ['categories'] });
     },
   });
 
