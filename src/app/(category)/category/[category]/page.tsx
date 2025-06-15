9a9e5e1901b1f6057252faf598f77fdf9358bfa4
LeeYunHwan <cyzhqly@gmail.com>
2025-06-15 22:27:51 +0900

    refactor: 카테고리별 메모 페이지 클라이언트에서 서버 컴포넌트로 변경 #101

--- src/app/(category)/category/[category]/page.tsx
+++ src/app/(category)/category/[category]/page.tsx
@@ -1,20 +1,27 @@
-'use client';
+import MemoSection from '@/components/memo/MemoSection';// 서버에서 memos를 가져오는 함수
+import type { MemoProps } from '@/types/memo';
+import { cookies } from 'next/headers';
 
-import MemoSection from '@/components/memo/MemoSection';
-import useGetMemos from '@/hooks/useGetMemos';
-import { useParams } from 'next/navigation';
-import React from 'react';
+interface PageProps {
+  params: { category: string };
+}
 
-export default function EachCategoryPage() {
-  const { data: memos = [] } = useGetMemos();
-  const { category } = useParams();
+async function getMemos(): Promise<MemoProps[]> {
+  const cookieStore = cookies();
+  const res = await fetch(`http://localhost:3000/api/memos`, {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+    cache: 'no-store',
+  });
+  return res.json();
+}
 
-  if (typeof category !== 'string') {
-    return <div>잘못된 경로입니다.</div>;
-  }
+export default async function EachCategoryPage({ params }: PageProps) {
+  const memos = await getMemos();
 
-  const decodedCategory = decodeURIComponent(category);
-
+  const decodedCategory = decodeURIComponent(params.category);
   const filteredMemo = memos.filter(memo => memo.category === decodedCategory);
+
   return <MemoSection memos={filteredMemo} />;
 }
