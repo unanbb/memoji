c95a9176afb950c75b019a7821b819efc7240f01
LeeYunHwan <cyzhqly@gmail.com>
2025-06-16 23:19:54 +0900

    fix: 반환 타입 Awaited 적용 #101

--- src/lib/fetchers.ts
+++ src/lib/fetchers.ts
@@ -1,6 +1,6 @@
 import type { cookies } from 'next/headers';
 
-export async function fetchMemos(cookieStore?:  ReturnType<typeof cookies>) {
+export async function fetchMemos(cookieStore?: Awaited<ReturnType<typeof cookies>>) {
   const headers: HeadersInit = {};
 
   if (cookieStore) {
