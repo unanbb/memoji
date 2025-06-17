212ba9ac5cef093b62a39703debcda6953222be5
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-06-17 17:10:58 +0900

    fix: ClientHome에서 검색 쿼리 디바운스 적용
    - 검색 쿼리에 디바운스를 적용
    - 디바운스 동안, 로딩 스켈레톤을 표시

--- src/components/ClientHome.tsx
+++ src/components/ClientHome.tsx
@@ -3,18 +3,21 @@ import ProtectedRoute from '@/components/auth/ProtectedRoute';
 import MemoListSkeleton from '@/components/common/MemoListSkeleton';
 import MemoSection from '@/components/memo/MemoSection';
 import { NotFondMemos } from '@/components/NotFoundMemos';
+import useDebounce from '@/hooks/useDebounce';
 import useSearchMemos from '@/hooks/useSearchMemos';
 import { useSearchStore } from '@/store/SearchStore';
 
 export default function ClientHome() {
   const searchQuery = useSearchStore(state => state.searchQuery);
 
+  const { debouncedValue, isDebouncing } = useDebounce(searchQuery, 300);
+
   const { isLoading, data: memos = [] } = useSearchMemos({
-    search: searchQuery,
+    search: debouncedValue,
     category: '',
   });
 
-  if (isLoading) {
+  if (isLoading || isDebouncing) {
     return <MemoListSkeleton />;
   }
 
