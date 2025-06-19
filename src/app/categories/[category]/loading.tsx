8c4a2b2ade86d1a450bff3974283e9b5a5ba7db2
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-06-19 17:21:47 +0900

    feat: not-found 페이지와 로딩 페이지 추가

---
+++ src/app/categories/[category]/loading.tsx
@@ -0,0 +1,5 @@
+import MemoListSkeleton from '@/components/common/MemoListSkeleton';
+
+export default function loading() {
+  return <MemoListSkeleton />;
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,32 @@
+import { ROUTE } from '@/components/constants/path';
+import Link from 'next/link';
+import { IoHomeOutline } from 'react-icons/io5';
+
+export default function NotFound() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br   flex flex-col items-center justify-center p-4">
+      <div className="text-center space-y-6 max-w-md">
+        <div className="space-y-2">
+          <h1 className="text-8xl font-black text-gray-900 tracking-tight">404</h1>
+          <div className="w-24 h-1 bg-black mx-auto"></div>
+        </div>
+
+        <div className="space-y-3">
+          <h2 className="text-2xl font-bold text-gray-800">페이지를 찾을 수 없습니다</h2>
+          <p className="text-gray leading-relaxed">
+            요청하신 페이지가 존재하지 않거나
+            <br />
+            이동되었을 수 있습니다.
+          </p>
+        </div>
+        <Link
+          className="inline-flex items-center px-6 py-3 bg-black text-white font-medium rounded-lg hover:bg-gray-800 transition-colors duration-200 shadow-lg hover:shadow-xl transform hover:-translate-y-0.5"
+          href={ROUTE.HOME}
+        >
+          <IoHomeOutline className="w-4 h-4 mr-2" />
+          홈으로 돌아가기
+        </Link>
+      </div>
+    </div>
+  );
+}
