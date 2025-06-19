c0e754898bbd6381d1f7a5049b8fab404f6d2442
LeeYunHwan <cyzhqly@gmail.com>
2025-06-19 22:02:06 +0900

    fix: 동일한 카테고리에 대해 대소문자 관계없이 api 호출이 안되도록 수정 #136

--- src/components/category/CategoryModal.tsx
+++ src/components/category/CategoryModal.tsx
@@ -167,7 +167,7 @@ export default function CategoryModal({ onClose }: { onClose: () => void }) {
           ),
         );
         return;
-      } else if (categoryName === newCategoryName){
+      } else if (categoryName.toLowerCase() === newCategoryName.toLowerCase()){
         setCategoryStates(prev =>
           prev.map((state, i) =>
             i === index ? { ...state, isEditing: false } : state,
